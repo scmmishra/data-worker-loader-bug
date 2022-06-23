a4a66c0d99442bc2bf7af0c50a272d39fe419796
Shivam Mishra <scm.mymail@gmail.com>
2022-06-23 16:39:39 +0530

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,43 @@
+export default {
+  /*
+   ** Headers of the page
+   ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
+   */
+  head: {
+    title: "Nuxt.js starter for CSB",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Official Nuxt.js starter for CodeSandBox"
+      }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+
+  components: true,
+  plugins: ["~/plugins/worker.client.js"],
+  build: {
+    publicPath: "https://cdn.nuxtjs.org/",
+    extend(config, { isClient }) {
+      config.output.globalObject = "this";
+
+      if (isClient) {
+        config.module.rules.push({
+          test: /\.worker\.(c|m)?js$/i,
+          use: [
+            {
+              loader: "worker-loader",
+              options: {
+                publicPath: "/worker-not-from-default/"
+              }
+            }
+          ],
+          exclude: /(node_modules)/
+        });
+      }
+    }
+  }
+};
---
+++ plugins/worker.client.js
@@ -0,0 +1,5 @@
+import DataWorker from "~/assets/data.worker.js";
+
+const worker = new DataWorker();
+
+console.log(worker);
